46af8be3f18fd7492a445f57300f8203542fd3b5
Claire Lee <jclairelee2@gmail.com>
2026-02-04 11:15:25 -0800

    Implement restaurant sorting by restaurant_id

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -8,8 +8,36 @@ const Restaurant = require("../models/Restaurant");
  */
 router.get("/", async (req, res) => {
   try {
-    const data = await Restaurant.find();
-    res.json(data);
+    const sortBy = req.query.sortBy;
+
+    // If no sorting, return all
+    if (!sortBy) {
+      const data = await Restaurant.find();
+      return res.json(data);
+    }
+
+    const order = sortBy === "DESC" ? -1 : 1;
+
+    const data = await Restaurant.find(
+      {},
+      {
+        _id: 1,
+        cuisine: 1,
+        name: 1,
+        borough: 1,
+        restaurant_id: 1,
+      },
+    ).sort({ restaurant_id: order });
+
+    const result = data.map((r) => ({
+      id: r._id,
+      cuisines: r.cuisine,
+      name: r.name,
+      city: r.borough,
+      restaurant_id: r.restaurant_id,
+    }));
+
+    res.json(result);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
